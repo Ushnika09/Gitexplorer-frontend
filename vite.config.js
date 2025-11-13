931bc543bba312159d25792c4d9289d6377148b4
Ushnika09 <karushnika@gmail.com>
2025-11-13 22:09:30 +0530

    try

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: './', // 👈 crucial for relative asset paths during deployment
+  build: {
+    outDir: 'build', // optional: match Netlify's publish directory
+  },
 })
